cc72c05af6d77ce773247d4de85f0a6536fe38c1
pawanjatav <pawan.j@cisinlabs.com>
2017-06-15 20:29:14 +0530

    implemente code for video

--- src/pages/Quran/Quran.ts
+++ src/pages/Quran/Quran.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
+import { Platform } from 'ionic-angular';
 
+import {SafeResourceUrl, DomSanitizer} from '@angular/platform-browser';
 @Component({
     selector: 'page-Quran',
   templateUrl: 'Quran.html'
 })
 export class QuranPage {
     taboptionss: string;
-  constructor(public navCtrl: NavController) {
+        videoUrl: SafeResourceUrl;
+    constructor(
+        public navCtrl: NavController,
+        private platfrom: Platform,
+        private domSanitizer: DomSanitizer,
+        private youtube: YoutubeVideoPlayer
+    ) {
       var self = this;
-      self.taboptionss = "Quran";
+      self.taboptionss = "Quran";        
+ 
+      self.platfrom.ready().then((redy) => {
+         // self.videoUrl = self.domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/o06VNoOqzg8')
+
+          //YoutubeVideoPlayer.openVideo("https://www.youtube.com/embed/o06VNoOqzg8");
+          self.youtube.openVideo("https://www.youtube.com/watch?v=93ZU6j59wL4");
+      });
   }
 
 }
